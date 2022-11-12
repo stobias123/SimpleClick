2349c9b0cc6ba420aa2f72d5ddd8cfc79578ed97
Steven Tobias <stobias123@gmail.com>
2022-11-12 10:40:19 -0800

    can I add setup?

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='SimpleClick',
+    version='',
+    packages=['isegm', 'isegm.model', 'isegm.model.modeling', 'isegm.model.modeling.hrformer_helper',
+              'isegm.model.modeling.hrformer_helper.hrt', 'isegm.model.modeling.hrformer_helper.hrt.modules',
+              'isegm.model.modeling.transformer_helper', 'isegm.model.modeling.swin_transformer_helper'],
+    url='',
+    license='',
+    author='steven.tobias',
+    author_email='',
+    description=''
+)
